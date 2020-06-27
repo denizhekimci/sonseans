46ae09e72861011df34e62ad4ed19c9ffd9cfa98
dhekimci <dhekimci@konumsal.com.tr>
2020-06-27 10:29:13 +0300

    duzeltme3

---
+++ crawler/sonseans.js
@@ -0,0 +1,47 @@
+const got       = require('got');
+const cheerio   = require('cheerio');
+
+const PROTOCOL = "https:";
+const HOST = "https://www.trt.net.tr";
+
+const URL = 'http://www.trt.net.tr/televizyon/akis.aspx?kanal=trt-2&gun=0';
+
+function fetch(){
+
+    return got(URL).then(function(data){
+
+        var $ = cheerio.load(data.body,{decodeEntities:false});
+
+        var yayinAkisi = {
+            claim: URL
+        }
+        let txt = rp(url)
+            .then(function (html) {
+                let movieTimes = $('#gunlukAkisDIV > p.tur97 > a > span.aks0 ', html).map(function () {
+                    return $(this).text();
+                }).toArray();
+
+                let movieTitles = [];
+                movieTitles = $('#gunlukAkisDIV > p.tur97 > a > span.aks1 ', html).map(function () {
+                    return $(this).text();
+                }).toArray();
+
+                for (let i = 0; i < movieTitles.length; i++) {
+                    yayinAkisi += movieTimes[i] + ' - ' + movieTitles[i] + '\n';
+                }
+                var resultText = 'TRT2\'de bugünkü filmler:\n' + yayinAkisi
+
+                return resultText;
+            }).catch(function (err) {
+                //handle error
+                return err;
+            });
+
+
+        return yayinAkisi;
+    });
+}
+
+module.exports = {
+    fetch : fetch
+};
